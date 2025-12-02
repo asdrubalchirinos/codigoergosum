e5010f287d5d737dd3be768558df97afa9cb124b
Asdrúbal Chirinos <achirinos@gmail.com>
2025-12-02 17:49:20 -0600

    feat: Introduce PostReference component, and enable MDX support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   // Dominio canonical normalizado (usar siempre www para evitar 301 extras)
   site: 'https://www.codigoergosum.com',
   // base removido porque el sitio está en la raíz
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   output: 'static',
   build: {
     format: 'directory',
