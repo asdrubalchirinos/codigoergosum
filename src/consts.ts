c4e5c9ddcfead6c7ea8cc8aac6aa3f68f63c5ced
Asdrúbal Chirinos <achirinos@gmail.com>
2025-12-04 16:23:13 -0600

    feat: update site description and default Open Graph image

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Código Ergo Sum';
-export const SITE_DESCRIPTION = 'Blog minimalista desarrollado con Astro.';
+export const SITE_DESCRIPTION = 'Descubre en Código ergo sum cómo los cambios laborales y tecnológicos están transformando los negocios. Exploramos desarrollo de software, gestión del tiempo e impacto de la inteligencia artificial. Únete y navega el panorama laboral con éxito.';
